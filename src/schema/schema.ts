f3615ae1fe94f4042830ff045781ac1e5de7b5b7
Daniel <dan.sldv@gmail.com>
2018-07-16 18:20:30 +0300

    Add authorId to books

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,13 +1,27 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID } from 'graphql';
-
+import { authors } from './../storage/authors';
+import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 import { books } from '../storage';
+
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: ({ authorId }, args) => authors.find((author) => author.id === authorId)
+    }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -18,6 +32,11 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve: (parent, { id }) => books.find((book) => book.id === id)
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, { id }) => authors.find((author) => author.id === id)
     }
   }
 });
---
+++ src/storage/authors.ts
@@ -0,0 +1,5 @@
+export const authors: any[] = [
+  { id: '1', name: 'Fyodor Dostoevsky', age: 51},
+  { id: '2', name: 'Alexandre Dumas', age: 56},
+  { id: '3', name: 'Joan Rowling', age: 71}
+]
